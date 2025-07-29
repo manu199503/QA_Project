6ef9ab53ccbb7bb8ad1dbe11e35475e8a785e80d
Manogna <nmanogna95@gmail.com>
2025-07-29 13:07:47 +0200

    Update test_02_register_negative.py

--- frontend/tests/test_02_register_negative.py
+++ frontend/tests/test_02_register_negative.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_register_with_existing_email(driver):
     driver.get("http://localhost:5173")
-
-    driver.find_element(By.ID, "exampleInputname").send_keys("pk")
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "exampleInputname")))
+    element.send_keys("pk")
     driver.find_element(By.ID, "exampleInputEmail1").send_keys("pvsspkr@gmail.com")  # already used
     driver.find_element(By.ID, "exampleInputPassword1").send_keys("pru12")
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
