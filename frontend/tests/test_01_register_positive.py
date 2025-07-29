039a5a1d163a5f1c867557e156870e8550c11a68
Manogna <nmanogna95@gmail.com>
2025-07-29 13:07:12 +0200

    Update test_01_register_positive.py

--- frontend/tests/test_01_register_positive.py
+++ frontend/tests/test_01_register_positive.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 def test_register(driver):
     driver.get("http://localhost:5173")
-    driver.find_element(By.ID, "exampleInputname").send_keys("pk")
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "exampleInputname")))
+    element.send_keys("pk")
     driver.find_element(By.ID, "exampleInputEmail1").send_keys("pvsspkr@gmail.com")
     driver.find_element(By.ID, "exampleInputPassword1").send_keys("pru12")
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
